7871546fc35bb80fc3305afcd1c2f16e7dc16a3d
Santiago G <san7iagogr.05@gmail.com>
2025-05-17 16:47:46 -0500

    (feat) Services : Terminando Aplicaciones

--- lib_aplicaciones/Interfaces/IVehiclesAplicacion.cs
+++ lib_aplicaciones/Interfaces/IVehiclesAplicacion.cs
@@ -7,7 +7,7 @@ namespace lib_aplicaciones.Interfaces
     public interface IVehiclesAplicacion
     {
         void Configurar(string StringConexion);
-        List<Vehicles> PorPlate(Vehicles? entidad);
+        List<Vehicles> PorPlaca(Vehicles? entidad);
         List<Vehicles> Listar();
         Vehicles? Guardar(Vehicles? entidad);
         Vehicles? Modificar(Vehicles? entidad);
