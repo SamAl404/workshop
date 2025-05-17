1741d9ada900a7384248d1a0932d91fb58a9a734
SamAl404 <samualvarezh@gmail.com>
2025-05-17 18:23:50 -0500

    cache

--- ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
+++ ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128c026f892241575d55ef70589a6ea3136a915d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbaa7c4bffd2971764245eaabee182005dbfbeb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
